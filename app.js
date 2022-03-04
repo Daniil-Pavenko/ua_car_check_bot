33fe52d71874bce839a57fc5b643bfe69ee869ab
Daniil Pavenko <daniil.pavenko@gmail.com>
2022-03-04 14:37:39 +0200

    Setup handle not found flow

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import { Telegraf } from 'telegraf';
 import dotenv from 'dotenv';
 import axios from 'axios';
+import utf8 from 'utf8';
 
 
 dotenv.config();
@@ -26,11 +27,11 @@ bot.on('text', async (ctx) => {
 
 	if (isCorrectNumber) {
 		ctx.reply('Почекайте хвилинку, шукаємо інформацію')
-		axios.get(`https://opendatabot.com/api/v3/public/transport?number=${numberOfCar.toUpperCase()}`)
+		axios.get(`https://opendatabot.com/api/v3/public/transport?number=${utf8.encode(numberOfCar.toUpperCase())}`)
 			.then(function (response) {
 				let car = response.data
 				if (car == undefined || car == null) {
-					ctx.reply(`Автомобіль за номером ${numberOfCar} не знайдений. Обережно!!!`)
+					ctx.replyWithHTML(`Автомобіль за номером <b>${numberOfCar}</b> не знайдений. Обережно!!!`)
 					return
 				}
 				let info = `<u>Знайдено за номером ${numberOfCar}</u>\n<b>Модель:</b> ${car.model}\n<b>Рік:</b> ${car.year}\n<b>Дата реєстрації:</b> ${car.date}\n<b>Колір:</b> ${car.color}\n<b>Тип:</b> ${car.body}\n<b>Реєстрація:</b> ${car.dep}`
@@ -38,7 +39,11 @@ bot.on('text', async (ctx) => {
 		  	})
 		  	.catch(function (error) {
 				console.log(error);
-				ctx.reply('Не корректний формат номеру авто, англійськими буквами. Приклад: AA1111KK')
+				if (error.response.status == 404) {
+					ctx.replyWithHTML(`Автомобіль за номером <b>${numberOfCar}</b> не знайдений. Обережно!!!`)
+				} else {
+					ctx.reply('Не корректний формат номеру авто, англійськими буквами. Приклад: AA1111KK')
+				}
 		  	})
 		  	.then(function () {
 				// always executed
